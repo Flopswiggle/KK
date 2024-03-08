706f18529c96e1be5214bbc21286e10d8c4652dc
Flopswiggle <156843814+Flopswiggle@users.noreply.github.com>
2024-03-08 13:24:22 -0700

    Horselist and Horse file

---
+++ HORSE/Horselist.java
@@ -0,0 +1,20 @@
+//Simple horse class created by Kim Gross
+//for CTE Software Development class 2022
+
+public class Horselist {
+    public static void Main(String[] args){
+        Horse horse1 =new Horse("red",2001);
+            Horse horse2 =new Horse("Sliver",2001);
+                Horse horse3 =new Horse("Bron",2001);
+                    System.out.println(horse1);
+                        System.out.println(horse2);
+                            System.out.println(horse3);
+
+
+
+    }
+
+
+
+
+}
